fd3778c4c166ce84e09b730eb024b137157fba47
Erhan Bagdemir <erhan.bagdemir@googlemail.com>
2014-11-19 00:14:31 +0100

    copyright url fix

--- src/io/moo/container/defs.clj
+++ src/io/moo/container/defs.clj
@@ -1,6 +1,6 @@
 ; The MIT License (MIT)
 ; 
-; Copyright (c) 2014 mooito.org - Erhan Bagdemir
+; Copyright (c) 2014 moo.io - Erhan Bagdemir
 ; 
 ; Permission is hereby granted, free of charge, to any person obtaining a copy
 ; of this software and associated documentation files (the "Software"), to deal

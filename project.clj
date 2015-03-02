2ec21e77a693647aa9f85f698e3c37812bfa3b32
Erhan Bagdemir <bagdemir@adobe.com>
2015-03-02 12:05:33 +0100

    resource path set explicitly

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/mooito/moo"
   :license {:name "MIT License" :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main io.moo.container.container)
+  :main io.moo.container.container
+  :resource-paths ["src/main/resource"])

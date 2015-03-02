d4dcba5239e8bf29b9ebc13858beead707e04dd8
Erhan Bagdemir <bagdemir@adobe.com>
2015-03-02 12:01:54 +0100

    Project informations fixed

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject moo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Command line console implementation for moo-framework."
+  :url "https://github.com/mooito/moo"
+  :license {:name "MIT License" :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main io.moo.container.container)

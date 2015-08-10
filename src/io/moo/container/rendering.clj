d688629ac82ad9689502ec1350e0e63ad2979c94
Bagdemir <erhan.bagdemir@googlemail.com>
2015-08-11 00:46:25 +0200

    tabular renderer function signature

--- src/io/moo/container/rendering.clj
+++ src/io/moo/container/rendering.clj
@@ -1,5 +1,15 @@
 (ns io.moo.container.rendering)
 
 (defmulti render (fn [x] (:format x)))
+
+;; Plain output rendering.
 (defmethod render "plain" [input]
   (:output input))
+
+;; Tabular representation rendering.
+(defmethod render "table" [input]
+  (let [cols (:cols input)
+        rows (:rows input)
+        headers (:headers input)
+        output-matrix (:output input)
+        ]))

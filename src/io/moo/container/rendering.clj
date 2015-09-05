9d0b60154421a19933dbe945f75d4807ecb7bf1c
Bagdemir <erhan.bagdemir@googlemail.com>
2015-09-05 12:56:07 +0200

    fix for infinite loop

--- src/io/moo/container/rendering.clj
+++ src/io/moo/container/rendering.clj
@@ -1,15 +1,22 @@
 (ns io.moo.container.rendering)
 
-(defmulti render (fn [x] (:type x)))
+(defmulti render
+  (fn [input, metadata] (:type metadata)))
 
-;; Plain output rendering.
-(defmethod render "plain" [input]
-  (:output input))
+(defmethod render "plain" [input, metadata]
+  (format (:format metadata) (first input) ))
 
 ;; Tabular representation rendering.
-(defmethod render "table" [input]
-  (let [cols (:cols input)
-        hlabels (:labels (:headers input))
-        hformat (:format (:headers input))
-        cformat (:format (:columns input))
-        ]))
+(defmethod render "table" [input, metadata]
+  (let [cols (:cols metadata)
+        hlabels (:headers (:labels metadata))
+        hformat (:headers (:format metadata))
+        cformat (:columns (:format metadata))]
+    
+    (loop [cell input, row nil]
+      (println cell)
+      (if (not (empty? cell))
+        (if (> (count row) cols)
+          (do (recur (rest cell) (conj row (first input))))
+          (recur (rest cell) nil)
+          )))))
--- test/io/moo/container/rendering_test.clj
+++ test/io/moo/container/rendering_test.clj
@@ -2,22 +2,22 @@
   (:use clojure.test)
   (:require [io.moo.container.rendering :as renderer]))
 
+(def number-of-columns 3)
 
 (deftest output-plain
-  (is (= "output" (renderer/render {:type "plain" :output "output"}))))
+  (is
+   (= "content"
+      (renderer/render
+       ["content"] {:type "plain" :format "%s"}))))
 
 (deftest output-tabular
   (is
    (= "<test-should-fail>"
       (renderer/render
-       {:cols 3,
-        :labels {
-                 :headers ["column1" "column2" "column3"]
-                 },
+       ["content1", "content2", "content3", "content4", "content" , "content6", "content7"],
+       {:cols number-of-columns,
+        :labels {:headers ["column1" "column2" "column3"]},
         :type "table",
-        :format {
-                 :headers [],
-                 :columns []
-                 }
-        }
-       ))))
+        :format {:headers ["format1", "format2", "format3"],
+                 :columns ["format1", "format2", "format3"]
+                 }}))))

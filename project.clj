53651800afa7e312125758d94493ac8e04d5b874
Erhan Bagdemir <erhan.bagdemir@googlemail.com>
2015-09-12 14:10:27 +0200

    version fixed to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.moo/tars "1.0.0-RC"
+(defproject io.moo/tars "0.1.0"
   :description "CLI framework for Clojure."
   :url "https://github.com/mooito/tars"
   :license {:name "MIT License" :url "http://opensource.org/licenses/MIT"}
